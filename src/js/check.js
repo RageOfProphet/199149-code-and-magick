61aff151f45381b4686ef271d80ca295fe588759
RageOfProphet <rvlife8@gmail.com>
2016-10-30 13:55:30 +0300

    task2 is ok

---
+++ src/js/check.js
@@ -0,0 +1,27 @@
+function getMessage (a, b) {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Я попал в ' + b;
+    } else {
+      return 'Я никуда не попал';
+    }
+  } else if (typeof a === 'number') {
+    return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+  } else if (Array.isArray(a) && !(b instanceof Array)) {
+  /*  var numberOfSteps = 0
+    for (var i = 0; i < a.lenght; i++) {
+      numberOfSteps += a[i]
+    } */
+    return 'Я прошел ' + numberOfSteps + ' шагов'
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+/*   var distancePath = 0
+ var Composition = 0
+  for (var i = 0; i < a.lenght; i++) {
+      Composition = a[i] * b[i]
+      distancePath += Composition
+    } */
+    return 'Я прошел ' + distancePath + ' метров';
+  } else {
+    return 'Переданы некорректные данные';
+  }
+}
