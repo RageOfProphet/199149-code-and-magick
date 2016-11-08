6850ad600d463a7657d24a6e71ea54c1bc22c156
RageOfProphet <rvlife8@gmail.com>
2016-11-08 20:12:06 +0300

    Работающая форма отзыва с валидируемой кнопкой

--- src/js/form.js
+++ src/js/form.js
@@ -1,36 +1,52 @@
 'use strict';
 
-var nameNeeded = document.getElementById('review-name');
-nameNeeded.setAttribute('required', 'true');
+var reviewName = document.getElementById('review-name');
+var reviewText = document.querySelector('.review-form-field-text');
+var reviewSubmit = document.querySelector('.review-submit');
+
+var hideName = document.querySelector('.review-fields-name');
+//nameIsWritten.setAttribute('required', 'true');
+var hideReview = document.querySelector('.review-fields-text');
+var bothIsWritten = document.querySelector('.review-fields');
 
 var reviewMarks = document.querySelectorAll('input[name="review-mark"]');
-console.log(reviewMarks);
-for (var i = 0; i < reviewMarks.length; i++) {
-  reviewMarks[i].onclick = function(e) {
-    console.log(e.target.value);
 
-    var checkedValue = document.querySelector('input[name="review-mark"]:checked').value;
-    if (+checkedValue < 3) {
-      var requiredText = document.getElementById('review-text');
-      requiredText.setAttribute('required', 'true');
-    }
+for (var i = 0; i < reviewMarks.length; i++) {
+  reviewMarks[i].onclick = function() {
+    reviewSubmit.disabled = validateForm();
   };
 }
 
-var nameIsWritten = document.querySelector('.review-fields-name');
-console.log(nameIsWritten.value);
-if ((nameIsWritten.value) !== '') {
-  nameIsWritten.setAttribute('class', 'invisible');
-  var firstCond = true;
-} else {
-  firstCond = false;
-}
-var reviewIsWritten = document.querySelector('.review-fields-text');
-if ((reviewIsWritten.value) !== '') {
-  reviewIsWritten.setAttribute('class', 'invisible');
-  var secondCond = true;
-} else {
-  secondCond = false;
+reviewName.oninput = function() {
+  reviewSubmit.disabled = validateForm();
+};
+
+reviewText.oninput = function() {
+  reviewSubmit.disabled = validateForm();
+};
+
+function validateForm() {
+  var isDisabled = true;
+  var checkedValue = parseInt(document.querySelector('input[name="review-mark"]:checked').value, 10);
+  bothIsWritten.classList.remove('invisible');
+  hideReview.classList.remove('invisible');
+  hideName.classList.remove('invisible');
+  if(checkedValue < 3) {
+    if((reviewName.value !== '') && (reviewText.value !== '')) {
+      bothIsWritten.classList.add('invisible');
+      isDisabled = false;
+    } else if (reviewText.value !== '') {
+      hideReview.classList.add('invisible');
+    } else if (reviewName.value !== '') {
+      hideName.classList.add('invisible');
+    }
+  } else if (reviewName.value !== '') {
+    bothIsWritten.classList.add('invisible');
+    isDisabled = false;
+  } else if (reviewText.value !== '') {
+    hideReview.classList.add('invisible');
+  }
+  return isDisabled;
 }
 
 window.form = (function() {
