57c3626482eae672b5aa400d759c0e153dcf082b
RageOfProphet <rvlife8@gmail.com>
2016-11-14 17:39:12 +0300

    Подгрузка массива отзывов из jsonp

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -14,7 +14,7 @@ var load = function(url, callback, callbackName) {
   if (!callbackName) {
     callbackName = 'cb' + Date.now();
   }
-  window.callbackName = function(data) {
+  window[callbackName] = function(data) {
     callback(data);
   };
 
