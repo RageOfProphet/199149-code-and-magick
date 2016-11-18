afd2b01d62db968fba75c42531330f69aef1fc06
RageOfProphet <rvlife8@gmail.com>
2016-11-18 06:36:19 +0400

    начало 5го задания

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,73 +1,25 @@
 'use strict';
 
-var container = document.querySelector('.reviews-list');
-var template = document.querySelector('#review-template');
-var templateContainer = 'content' in template ? template.content : template;
+(function() {
+  var container = document.querySelector('.reviews-list');
 
-var reviewsFilter = document.querySelector('.reviews-filter');
+  var loadUrl = 'http://localhost:1507/api/reviews';
 
-var IMAGE_LOAD_TIMEOUT = 3000;
-
-var loadUrl = 'http://localhost:1507/api/reviews';
-
-var load = function(url, callback, callbackName) {
-  if (!callbackName) {
-    callbackName = 'cb' + Date.now();
+/*  function hideFilters() {
+    reviewsFilter.classList.add('invisible');
   }
-  window[callbackName] = function(data) {
-    callback(data);
+  hideFilters();*/
+
+  var renderReviews = function(reviews) {
+    reviews.forEach(function(review) {
+      container.appendChild(getReviewElement(review));
+    });
   };
 
-  var script = document.createElement('script');
-  script.src = url + '?callback=' + callbackName;
-  document.body.appendChild(script);
-};
+/*  function showFilters() {
+    reviewsFilter.classList.remove('invisible');
+  }
+  showFilters();*/
 
-function hideFilters() {
-  reviewsFilter.classList.add('invisible');
-}
-hideFilters();
-
-var getReviewElement = function(review) {
-  var reviewElement = templateContainer.querySelector('.review').cloneNode(true);
-  reviewElement.querySelector('.review-author').textContent = review.author.name;
-  reviewElement.querySelector('.review-rating').textContent = review.rating;
-  reviewElement.querySelector('.review-text').textContent = review.description;
-
-  var currentPicture = reviewElement.querySelector('img');
-
-  var loadedImage = new Image(124, 124);
-  var loadImageTimeout = 0;
-
-  loadedImage.src = review.author.picture;
-
-  loadedImage.onload = function(evt) {
-    clearTimeout(loadImageTimeout);
-    currentPicture.src = evt.target.src;
-  };
-
-  loadedImage.onerror = function() {
-    reviewElement.classList.add('review-load-failure');
-  };
-
-  loadImageTimeout = setTimeout(function() {
-    reviewElement.classList.add('review-load-failure');
-  }, IMAGE_LOAD_TIMEOUT);
-
-  return reviewElement;
-};
-
-var renderReviews = function(reviews) {
-  reviews.forEach(function(review) {
-    container.appendChild(getReviewElement(review));
-  });
-};
-
-function showFilters() {
-  reviewsFilter.classList.remove('invisible');
-}
-showFilters();
-
-load(loadUrl, renderReviews, 'jsonpCB');
-
-
+  load(loadUrl, renderReviews, 'jsonpCB');
+  })();
